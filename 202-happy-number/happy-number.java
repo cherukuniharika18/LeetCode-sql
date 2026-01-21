15b70b571afd9372071ca0a831fc169e5f77b9ec
cherukuniharika18 <cherukuniharika18@gmail.com>
2026-01-21 06:35:03 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (95.24%) - LeetSync

---
+++ 202-happy-number/happy-number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isHappy(int n) {
+        while(n!=1 && n!=4)
+        {
+            n=next(n);
+        }
+        return n==1;
+    }
+    private int next(int n)
+    {
+        int sum=0;
+        while(n>0)
+        {
+            int d=n%10;
+            sum+=d*d;
+            n/=10;
+        }
+        return sum;
+    }
+}

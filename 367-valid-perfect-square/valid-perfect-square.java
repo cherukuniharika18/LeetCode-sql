622a4cdc121c4503eb21210662a862f3fc6984b3
cherukuniharika18 <cherukuniharika18@gmail.com>
2026-04-29 16:20:37 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (73.94%) - LeetSync

---
+++ 367-valid-perfect-square/valid-perfect-square.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        if(num<0)
+        {
+            return false;
+        }
+        if(num==0 || num==1)
+        {
+            return true;
+        }
+        int low=1, high=num;
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+            long square=(long)mid*mid;
+            if(square==num)
+            {
+                return true;
+            }
+            else if(square<num)
+            {
+                low=mid+1;
+            }
+            else
+            {
+                high=mid-1;
+            }
+        }
+        return false;
+    }
+}

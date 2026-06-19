f4d823028b233f95fbb8c46d3564ac21f52bfe92
cherukuniharika18 <cherukuniharika18@gmail.com>
2026-06-19 07:23:30 +0530

    Time: 30 ms (5.03%) | Memory: 83.5 MB (76.12%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,15 +1,6 @@
-class Solution 
-{
-    public boolean containsDuplicate(int[] nums) 
-    {
-        Arrays.sort(nums);
-        for(int i=0;i<nums.length-1;i++)
-        {
-            if(nums[i]==nums[i+1])
-            {
-                return true;
-            }
-        }
-        return false;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        return Arrays.stream(nums).distinct().count() != nums.length;
     }
 }
